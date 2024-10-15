e14047682151353e751049b48c851de325baf3c8
Spudmanst <spudmanst@hotmail.com>
2024-10-15 14:29:19 +0000

    ROUNDS introduced. Fixed gaming looping too often. Tidied up screen when playing by creating less new lines.

--- Blackjack/Blackjack.py
+++ Blackjack/Blackjack.py
@@ -1,4 +1,4 @@
-# Blackjack version 1.0.5
+# Blackjack version 1.1.0
 
 import random
 import time
@@ -70,9 +70,7 @@ print(f"""
  |____/|_|\__,_|\___|_|\_\____/ \__,_|\___|_|\_(_)
                                                   """)
 
-def start_game():
-    cards = create_shuffled_deck()
-    
+def start_game(): 
     # Get number of players
     while True:
         try:
@@ -83,102 +81,124 @@ def start_game():
                 print("Invalid input, must use a number between 1 - 7 to continue.")
         except ValueError:
             print("Invalid input, please enter a number between 1 and 7.")
-
-    # Deal cards to players
-    player_hands = {player: [cards.pop() for _ in range(2)] for player in range(1, num_of_players + 1)}
-    dealer_hand = [cards.pop() for _ in range(2)]
-
-    # Calculate scores for players
-    player_score = {player: adjust_for_aces(player_hands[player]) for player in range(1, num_of_players + 1)}
-    dealer_score = adjust_for_aces(dealer_hand)
-
-    # Initialize player win status
-    player_win = {player: None for player in range(1, num_of_players + 1)}
-
-    # Players have their turn
-    for player in range(1, num_of_players + 1):
-        slow_type(f"Dealer's hand: {dealer_hand[0]} and unknown")
-        slow_type(f"Player {player}'s hand: {player_hands[player][0]} and {player_hands[player][1]}")
-        
-        if player_score[player] == 21:
-            slow_type("Blackjack!")
-            player_win[player] = "Blackjack"
-            divide_lines()
-            continue # End current player's turn
-        else: 
-            slow_type(f"Player {player}'s score: {player_score[player]}")
             
-        while player_score[player] < 21:
-            action = input("What would you like to do, '(H)it' or '(S)tick'?\n").lower()
-
-            if action in ("hit", "h"):
-                new_card = cards.pop()
-                player_hands[player].append(new_card)
-                player_score[player] += scores[new_card.split()[0]]
-                if player_score[player] > 21:
-                    player_score[player] = adjust_for_aces(player_hands[player])
-                slow_type(f"Card received: {new_card}\nNew score: {player_score[player]}")
-            elif action in ("stick", "s"):
-                slow_type(f"Player {player} is sticking with a score of {player_score[player]}")
+    while True:
+        try:
+            num_of_rounds = int(input("How many rounds would you like? Minimum 1, Maximum 10: "))
+            if 1 <= num_of_rounds <= 10:
                 break
-            elif action in ("exit", "e"):
-                print("Exiting game.")
-                exit()
             else:
-                print(f"Unknown command, type 'hit' or 'stick' to continue.\nType 'exit' to finish playing")
-
-        if player_score[player] > 21:
-            slow_type(f"Player {player} busts!")
-            player_win[player] = "Bust"
-        
-        elif player_score[player] == 21 and player_win[player] != "Blackjack":
-            slow_type(f"Player {player} is sticking with a score of {player_score[player]}")
-        
+                print("Invalid input, must use a number between 1 - 10 to continue.")
+        except ValueError:
+            print("Invalid input, please enter a number between 1 and 10.")
+    
+    round_number = 1
+    
+    while round_number <= num_of_rounds:
         divide_lines()
+        print(f"ROUND {round_number}")
+        divide_lines() 
+        cards = create_shuffled_deck()
+        sus_sleep()
+
+        # Deal cards to players
+        player_hands = {player: [cards.pop() for _ in range(2)] for player in range(1, num_of_players + 1)}
+        dealer_hand = [cards.pop() for _ in range(2)]
+
+        # Calculate scores for players
+        player_score = {player: adjust_for_aces(player_hands[player]) for player in range(1, num_of_players + 1)}
+        dealer_score = adjust_for_aces(dealer_hand)
+
+        # Initialize player win status
+        player_win = {player: None for player in range(1, num_of_players + 1)}
+
+        # Players have their turn
+        for player in range(1, num_of_players + 1):
+            slow_type(f"Dealer's hand: {dealer_hand[0]} and unknown")
+            slow_type(f"Player {player}'s hand: {player_hands[player][0]} and {player_hands[player][1]}")
+            
+            if player_score[player] == 21:
+                slow_type("Blackjack!")
+                player_win[player] = "Blackjack"
+                divide_lines()
+                continue # End current player's turn
+            else: 
+                slow_type(f"Player {player}'s score: {player_score[player]}")
+                
+            while player_score[player] < 21:
+                action = input("What would you like to do, '(H)it' or '(S)tick'? ").lower()
+
+                if action in ("hit", "h"):
+                    new_card = cards.pop()
+                    player_hands[player].append(new_card)
+                    player_score[player] += scores[new_card.split()[0]]
+                    if player_score[player] > 21:
+                        player_score[player] = adjust_for_aces(player_hands[player])
+                    slow_type(f"Card received: {new_card}\nNew score: {player_score[player]}")
+                elif action in ("stick", "s"):
+                    slow_type(f"Player {player} is sticking with a score of {player_score[player]}")
+                    break
+                elif action in ("exit", "e"):
+                    print("Exiting game.")
+                    exit()
+                else:
+                    print(f"Unknown command, type 'hit' or 'stick' to continue.\nType 'exit' to finish playing")
+
+            if player_score[player] > 21:
+                slow_type(f"Player {player} busts!")
+                player_win[player] = "Bust"
+            
+            elif player_score[player] == 21 and player_win[player] != "Blackjack":
+                slow_type(f"Player {player} is sticking with a score of {player_score[player]}")
+            
+            divide_lines()
+            std_sleep()
+
+        # Dealer plays
+        slow_type(f"Dealer reveals hand: {dealer_hand[0]} and {dealer_hand[1]}")
+
+        if dealer_score == 21:
+            slow_type("Dealer has Blackjack.")
+        else:
+            slow_type(f"Dealer's score = {dealer_score}")
+
         std_sleep()
 
-    # Dealer plays
-    slow_type(f"Dealer reveals hand: {dealer_hand[0]} and {dealer_hand[1]}")
-
-    if dealer_score == 21:
-        slow_type("Dealer has Blackjack.")
-    else:
-        slow_type(f"Dealer's score = {dealer_score}")
-
-    std_sleep()
-
-    while dealer_score < 17:
-        new_card = cards.pop()
-        dealer_hand.append(new_card)
-        dealer_score += scores[new_card.split()[0]]
-        dealer_score = adjust_for_aces(dealer_hand)
-        slow_type(f"Dealer receives: {new_card}\nDealer's new score: {dealer_score}")
-    
-    if dealer_score > 21:
-        slow_type("Dealer has busted!")
+        while dealer_score < 17:
+            new_card = cards.pop()
+            dealer_hand.append(new_card)
+            dealer_score += scores[new_card.split()[0]]
+            dealer_score = adjust_for_aces(dealer_hand)
+            slow_type(f"Dealer receives: {new_card}\nDealer's new score: {dealer_score}")
         
-    divide_lines() # Create divider when dealer has finished
-    std_sleep()
+        if dealer_score > 21:
+            slow_type("Dealer has busted!")
+            
+        divide_lines() # Create divider when dealer has finished
+        std_sleep()
 
-    # Calculate winners
-    for player in range(1, num_of_players +1):
-        if player_win[player] is None:
-            if dealer_score > 21:
-                player_win[player] = True
-                slow_type(f"Player {player} wins as the Dealer busted!")
-            elif player_score[player] > dealer_score and player_score[player] <= 21:
-                player_win[player] = True
-                slow_type(f"Player {player} wins with {player_score[player]} vs the Dealer's {dealer_score}!")
-            elif player_score[player] == dealer_score:
-                player_win[player] = "Tie"
-                slow_type(f"Player {player} ties with {player_score[player]}!")
+        # Calculate winners
+        for player in range(1, num_of_players +1):
+            if player_win[player] is None:
+                if dealer_score > 21:
+                    player_win[player] = True
+                    slow_type(f"Player {player} wins as the Dealer busted!")
+                elif player_score[player] > dealer_score and player_score[player] <= 21:
+                    player_win[player] = True
+                    slow_type(f"Player {player} wins with {player_score[player]} vs the Dealer's {dealer_score}!")
+                elif player_score[player] == dealer_score:
+                    player_win[player] = "Tie"
+                    slow_type(f"Player {player} ties with {player_score[player]}!")
+                else:
+                    player_win[player] = False
+                    slow_type(f"Player {player} loses with {player_score[player]} vs the Dealer's {dealer_score}!")
+            elif player_win[player] == "Blackjack":
+                slow_type(f"Player {player} wins with Blackjack!")
             else:
-                player_win[player] = False
-                slow_type(f"Player {player} loses with {player_score[player]} vs the Dealer's {dealer_score}!")
-        elif player_win[player] == "Blackjack":
-            slow_type(f"Player {player} wins with Blackjack!")
-        else:
-            slow_type(f"Player {player} busted!")
+                slow_type(f"Player {player} busted!")
+        
+        sus_sleep()        
+        round_number += 1
             
 
 while True:
@@ -188,7 +208,7 @@ while True:
 
     if action in ("replay", "r"):
         divide_lines()
-        start_game()
+        continue
     elif action in ("exit", "e"):
         print("Exiting game.")
         divide_lines()
